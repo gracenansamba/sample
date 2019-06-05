41faf4182a96dbb4c2e009730b4e98ca37293726
Nansamba <jpp751@mocs.utc.edu>
2019-06-05 15:51:40 -0400

    dot product cuda, fixing the loop

--- dots.cu
+++ dots.cu
@@ -7,17 +7,24 @@ using namespace std;
 
 __global__
 void dots(float *A, float *B, float *C, int M, int N, int K) 
-{
+
+{	
+	 printf("A : %.1f %.1f %.1f", A[0], A[1] ,A[2]);
+       	printf("B : %.1f %.1f %.1f", B[0] ,B[1], B[2]);
+	printf ("C: %.1f  %.1f %.1f", C[0] , C[1], C[2]);
+ 
+
 	int I = blockIdx.x * blockDim.x + threadIdx.x;
 	int J = blockIdx.y * blockDim.y + threadIdx.y;
 
 	if (I>=M || J >=N)
 		printf("threads are more than size");
+
 	
 	else 
 		{
 			int k; //where do  i set the k from? 
-			printf("A : %.1f", A[0]);
+			//printf("A : %d", A[0]);
 			
 			float temp = 0;
 			for (k=0; k<3; k++){
@@ -36,14 +43,15 @@ int main()
 
 	int M=3; int N=3; int K =3;
 /*
-	int A[M][K]= {1,2,3}; //can i do a dot like a matrix?
-	int B[K][N]= {1,2,3};
-	int C[M][N] = {0,0,0};
+	float A[M][K]= {1,2,3}; //can i do a dot like a matrix?
+	float B[K][N]= {1,2,3};
+	float C[M][N] = {0,0,0};
 */
 	//is this how i would do a normal dot
-	float A[M]= {1,2,3}; printf("A : % .1f  %.1f %.1f ", A[0], A[1], A[2]);
+	float  A[M]= {1,2,3};//printf("A : % d  %d %d ", A[0], A[1], A[2]);
+		//float *A = Aa; printf("A : % d  %d %d ", A[0], A[1], A[2]);
 	float B[N]= {1,2,3};
-	float C[K] = {0,0,0};
+	float C[3] = {0,0,0};
 	
 
 	
@@ -51,20 +59,20 @@ int main()
 	float *dA, *dB, *dC;
 	cudaMalloc(&dA, M*sizeof(float));
 	cudaMalloc(&dB, N*sizeof(float));
-	cudaMalloc(&dC, K*sizeof(float));
+	cudaMalloc(&dC, sizeof(float));
 
 //Copy memory to deveice
 
-	cudaMemcpy(&dA, A, sizeof(float), cudaMemcpyDeviceToHost);
-	cudaMemcpy(&dB, B, sizeof(float), cudaMemcpyDeviceToHost);
-	cudaMemcpy(&dC, C, sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(dA, &A[0], M* sizeof(float),cudaMemcpyHostToDevice);
+	cudaMemcpy(dB, &B[0], N*sizeof(float),cudaMemcpyHostToDevice);
+	cudaMemcpy(dC, &C[0], sizeof(float),cudaMemcpyHostToDevice);
 
 //call kernel
 	
 	dots<<<1,1>>>(dA,dB,dC,M,N,K);
 
 //Copy memory back to host
-	cudaMemcpy(C,&dC, sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(C,&dC, sizeof(float), cudaMemcpyDeviceToHost);
 	
 	cudaFree (dA);  cudaFree (dB);  cudaFree (dC);
 	return 0;
