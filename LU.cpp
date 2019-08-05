0ea5e81f3d312d3c5e94d2da2a1bffdffd15e75a
Grace Nansamba <gracen@southside.simcenter.utc.edu>
2019-08-05 14:26:35 -0400

    LU correct, but not auto or scalable

--- LU.cpp
+++ LU.cpp
@@ -2,6 +2,15 @@
 #include <string>
 #include <cmath>
 
+/*
+ this program can get the LU of the matrix "A [3][3] = {{1,2,3},{4,5,6},{7,8,10}}" 
+ the coeficients are k1=4, k2=7, k3=2
+ I calculated this before
+ I need to make the coefficients auto and also to make the 
+ equation solver robust and scalable
+ it's still very manual unscalable
+#brute forced for starters
+ */
 using namespace std;
 
 class LUFact{
@@ -12,101 +21,88 @@ class LUFact{
 			double A [3][3];
 			double U [3][3];
         public:
+			//this is a constructor
                         LUFact (double r1a,double r1b,double r1c,double r2a,double r2b,double r2c,double r3a,double r3b,double r3c) {
                                 this->r1a=r1a; this->r1b=r1b; this->r1c=r1c;
                                 this->r2a=r2a; this->r2b=r2b; this->r2c=r2c;
                                 this->r3a=r3a; this->r3b=r3b; this->r3c=r3c;
                         }
-                       /* double getR1a(){return r1a;}
-                        double getR1b(){return r1b;}
-                        double getR1c(){return r1c;}
-                        double getR2a(){return r2a;}
-                        double getR2b(){return r2b;}
-                        double getR2c(){return r2c;}
-                        double getR3a(){return r3a;}
-                        double getR3b(){return r3b;}
-                        double getR3c(){return r3c;}*/
+
+			//need a function to calculate the real LU 
+	
+
 			
-			//printf("value of A = %.1f\t", A[i][j]);
-			//row = 1;//create a loop to increment row++ to make them 3 or more
-                        void elimRow2(){
+                        double elimRow2 (double A[3][3], int k1 ){
 				for(int i=0; i<3; i++){
 				if(i==1){	
 				for(int j=0; j<3; j++){
                                 //if (r1a !=0)
-                                        U [i][j] =A[i][j]  - (k1*(A[i][j]));
-					printf("value of A = %.1f\t", A[i][j]);
+				//for(int k= 0; k<3; k++){
+                                       A [i][j] =A[i][j]  - (k1*(A[i-1][j]));
+					//printf("value of A = %.1f\t", A[i][j]);printf("\n"); printf("\n");
 
-
+	              		}
 				}
-                       		}
-			       	}
-
+				}
 
 				for (int i=0; i<3; i++){
                                 for (int j=0; j<3; j++)
                                 {
-                                        printf("%.1f\t", U[i][j]);
+                                        printf("%.1f\t", A[i][j]);
                                 }
                                         printf("\n"); printf("\n");
-                                }
-
-			}				
-                        void elimRow3(){
-                                //if (i==2){
+                                
+				}return A[3][3];
+				}
+				
+			
+			       				
+                        double elimRow3(double A[3][3], int k2 ){
+                                //f (i==2){
                                 for(int i=0; i<3; i++){
-                                //if(i==2){     
+                                if(i==2){     
                                 for(int j=0; j<3; j++){
                                 //if (r1a !=0)
-                                        U [i][j] =A[i][j]  - (k2*(A[i][j])); //printf("%.1f\t", U[i][j]);
+                                        A[i][j] =A[i][j]  - (k2*(A[i-2][j])); //printf("%.1f\t", U[i][j]);
 
                                 }
-                                //}
+                                }
                                 }
 
 
 				for (int i=0; i<3; i++){
 				 for (int j=0; j<3; j++)
                                 {
-                                        printf("%.1f\t", U[i][j]);
+                                        printf("%.1f\t", A[i][j]);
                                 }
                                         printf("\n"); printf("\n");
+                                }return A[3][3];
+			}
+
+			double elimRow3F(double A[3][3], int k3 ){
+                                //f (i==2){
+                                for(int i=0; i<3; i++){
+                                if(i==2){
+                                for(int j=0; j<3; j++){
+                                //if (r1a !=0)
+                                        A[i][j] =A[i][j]  - (k3*(A[i-1][j])); //printf("%.1f\t", U[i][j]);
+
+                                }
+                                }
                                 }
 
-			}
-				/*
-                        double ell(){           //if (r1b !=0)
-                                        return r2b = r2b - (k1*r1b);}
-                                //if (r1c !=0)
-                        double el(){    return r2c = r2c - (k1*r1c);}
-                        //}
 
+                                for (int i=0; i<3; i++){
+                                 for (int j=0; j<3; j++)
+                                {
+                                        printf("%.1f\t", A[i][j]);
+                                }
+                                        printf("\n"); printf("\n");
+                                }return A[3][3];
+                        }
 
-                        double elim2(){
-                                return r3a = r3a - (k2*r1a);}
-
-                        double ell2(){
-                                return r3b = r3b - (k2*r1b);}
-                        double el2(){
-                                return r3c = r3c - (k2*r1c);}
-
-                        double elim3(){
-                                return r3a = r3a - (k3*r2a);}
-                        double ell3(){
-                                        return r3c = r3c - (k3*r2c);}
-			double el3(){
-                                return r3c = r3c - (k3*r2c);}
-			double k;
-			void coeff (){
-
-				if (row >=0)
-				{
-					if (row= r1a||row= r1a|| row= r1a || row= r1a)
-						{row - k = 0; }
-					else //if row-k= 0 ; do nothing else find k as an unknown
-				}
-				
-			}*/
+			
+		
 };
 
 int main(){
@@ -114,7 +110,7 @@ int main(){
                          double r1a,r1b,r1c,r2a,r2b,r2c,r3a,r3b,r3c;
                          double k1, k2, k3;
 			
-	                cout << "please enter values of the 3 equations: "<< endl;
+	        	 cout << "please enter values of the 3 equations: "<< endl;
                          cin>> r1a; cout<< "";cin >>r1b;cout<< ""; cin>> r1c;cout<< "" <<endl;
                          cin>> r2a; cout<< "";cin>> r2b;cout<< ""; cin>> r2c;cout<< "" <<endl;
                          cin>> r3a; cout<< "";cin>> r3b;cout<< ""; cin>> r3c;cout<< "" <<endl;
@@ -125,36 +121,31 @@ int main(){
 			
 			 double A [3][3] = {{r1a,r1b,r1c},{r2a,r2b,r2c},{r3a,r3b,r3c}};
                          double U [3][3] = {{0,0,0},{0,0,0},{0,0,0}};
-			/*
-			 double A [3][3] = {{1,2,3},{4,5,6},{7,8,10}};
-                         double U [3][3] = {{0,0,0},{0,0,0},{0,0,0}};
-			*/
+			
+			  //printf("value of A below = %.1f\t", A[1][1]);
+
+			 //double A [3][3] = {{1,2,3},{4,5,6},{7,8,10}};
+                        // double U [3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+
 
  	                 LUFact one (r1a,r1b,r1c,r2a,r2b,r2c,r3a,r3b,r3c);
-                         one.elimRow2();
+                         cout << "first elimination" << endl;
+			 one.elimRow2(A,k1);
 		 	 cout << "second elimination" << endl;
-			 one.elimRow3();
-                        // one.elim2();
-                         //one.elim3();
+			 one.elimRow3(A,k2);
+			 cout << " The upper Triangular is " << endl;
+			 one.elimRow3F(A,k3);
 
-			/* for (int i=0; i<3; i++){
-				for (int j=0; j<3; j++)
-				{
-					printf("%.1f\t", U[i][j]);
+			 cout << "The lower triangular is "<< endl;
+			double L [3][3] ={{1,0,0},{k1,1,0},{k2,k3,1}};
+			for(int i=0;i<3;i++){
+				for (int j=0; j<3; j++){
+					//cout<< L[i][j];
+					 printf("%.1f\t",L[i][j]);
 				}
-					printf("\n"); printf("\n");
-				}
-			
-                        cout << "u1 = " << one.elim1() << endl;
-                        cout << "u2 = " << one.ell() << endl;
-	                cout << "u3 = " <<one.el() << endl;
-                        cout << "u4 = " <<one.elim2() << endl;
-                        cout << "u5 = " <<one.ell2() << endl;
-                        cout << "u6 = " <<one.el2() << endl;
-                        cout << "u7 = " <<one.elim3() << endl;
-                        cout << "u8 = " <<one.ell3() << endl;
-                        cout << "u9 = " <<one.el3() << endl;
-			*/
-                return 0;                                                          
+				printf("\n"); printf("\n");
+
+			}
+                      return 0;                                                          
  }
                             
