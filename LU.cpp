2675e2105669f6708cdb41a1e329355c2b809835
Grace Nansamba <gracen@southside.simcenter.utc.edu>
2019-08-09 16:20:26 -0400

    Works for A [3][3]

--- LU.cpp
+++ LU.cpp
@@ -3,13 +3,10 @@
 #include <cmath>
 
 /*
- this program can get the LU of the matrix "A [3][3] = {{1,2,3},{4,5,6},{7,8,10}}" 
- the coeficients are k1=4, k2=7, k3=2
- I calculated this before
- I need to make the coefficients auto and also to make the 
- equation solver robust and scalable
+ this program can get the LU of a [3][3] matrix
+ An equation solver should be robust and scalable
  it's still very manual unscalable
-#brute forced for starters
+ Working on that 
  */
 using namespace std;
 
@@ -21,7 +18,6 @@ class LUFact{
 			double A [3][3];
 			double U [3][3];
         public:
-			//this is a constructor
                         LUFact (double r1a,double r1b,double r1c,double r2a,double r2b,double r2c,double r3a,double r3b,double r3c) {
                                 this->r1a=r1a; this->r1b=r1b; this->r1c=r1c;
                                 this->r2a=r2a; this->r2b=r2b; this->r2c=r2c;
@@ -30,8 +26,6 @@ class LUFact{
 
 			//need a function to calculate the real LU 
 	
-			
-			//double kkk;
                         double elimRow2 (double A[3][3], double& k1 ){
 				//double kkk;
 				for(int i=0; i<3; i++){
@@ -41,7 +35,7 @@ class LUFact{
 				//for(int k= 0; k<3; k++){
 				if(j==0){
 					k1=(A[i][j])/(A[i-1][j]);
-					printf("value of k1 = %.1f \n ",k1);
+					//printf("value of k1 = %.1f \n ",k1);
 				} 
 				      // printf("value of k %.1f \n = " , k);	
 					//if(A[i][j]!=0 && j==0){
@@ -51,7 +45,7 @@ class LUFact{
 
 	              		}
 				}
-				}// kkk=k1;printf("kkk returned = %.1f \n",kkk);
+				}
 
 				for (int i=0; i<3; i++){
                                 for (int j=0; j<3; j++)
@@ -70,9 +64,8 @@ class LUFact{
                                 for(int j=0; j<3; j++){
 
 				if(j==0){
-                                //if (r1a !=0)
-					k2= (A[i][j])/(A[i-2][j]);
-					printf ("value of k2 for second elim =%.1f \n", k2);
+                                	k2= (A[i][j])/(A[i-2][j]);
+					//printf ("value of k2 for second elim =%.1f \n", k2);
 					
 					}
 					//printf("value of k for second elim is %.1f" , k);
@@ -80,10 +73,7 @@ class LUFact{
 					}
                                 }
                                 }
-                                
-
-
-				for (int i=0; i<3; i++){
+                                for (int i=0; i<3; i++){
 				 for (int j=0; j<3; j++)
                                 {
                                         printf("%.1f\t", A[i][j]);
@@ -100,10 +90,9 @@ class LUFact{
                                 //if (r1a !=0)
 				if (j==1){
 					k3 = (A[i][j])/(A[i-1][j]);
-					printf("value of k3 third elim = %.1f \n",k3);
+					//printf("value of k3 third elim = %.1f \n",k3);
 				}
                                         A[i][j] =A[i][j]  - (k3*(A[i-1][j])); //printf("%.1f\t", U[i][j]);
-
                                 }
                                 }
                                 }
@@ -114,36 +103,22 @@ class LUFact{
                                 }
                                         printf("\n"); printf("\n");
                                 }return A[3][3];
-                        }
-
-			
-		
+                        }	
 };
 
-int main(){
-			
+int main(){	
                          double r1a,r1b,r1c,r2a,r2b,r2c,r3a,r3b,r3c;
                         double k1, k2, k3,kkk;
-			printf("value of k1 in the  main = %.1f", k1);
+			//printf("value of k1 in the  main = %.1f", k1);
 	        	 cout << "please enter values of the 3 equations: "<< endl;
                          cin>> r1a; cout<< "";cin >>r1b;cout<< ""; cin>> r1c;cout<< "" <<endl;
                          cin>> r2a; cout<< "";cin>> r2b;cout<< ""; cin>> r2c;cout<< "" <<endl;
                          cin>> r3a; cout<< "";cin>> r3b;cout<< ""; cin>> r3c;cout<< "" <<endl;
-                      /*  cout << "please enter the coefients :"<< endl;
-                        cout << "k1= "; cin >> k1;
-                        cout << "k2= "; cin >> k2;
-                        cout << "k3= "; cin >> k3;*/
-			
+                      
 			 double A [3][3] = {{r1a,r1b,r1c},{r2a,r2b,r2c},{r3a,r3b,r3c}};
                          double U [3][3] = {{0,0,0},{0,0,0},{0,0,0}};
 			
-			  //printf("value of A below = %.1f\t", A[1][1]);
-
-			 //double A [3][3] = {{1,2,3},{4,5,6},{7,8,10}};
-                        // double U [3][3] = {{0,0,0},{0,0,0},{0,0,0}};
-
-
- 	                 LUFact one (r1a,r1b,r1c,r2a,r2b,r2c,r3a,r3b,r3c);
+		         LUFact one (r1a,r1b,r1c,r2a,r2b,r2c,r3a,r3b,r3c);
                          cout << "first elimination" << endl;
 			 one.elimRow2(A,k1);
 		 	 cout << "second elimination" << endl;
